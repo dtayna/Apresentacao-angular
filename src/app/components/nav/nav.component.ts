7219e776fa845716efb00c885c2c990ae9e53927
Debora Tayna <dtayna.3194@gmail.com>
2024-01-21 23:31:12 -0300

    hamburger menu

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  showMenu = false;
+
   constructor() { }
+
+  openMenu(){
+    this.showMenu = !this.showMenu;
+  }
 
   ngOnInit(): void {
   }
